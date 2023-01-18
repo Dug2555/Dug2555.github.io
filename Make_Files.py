a73e5894a4cef90cc49ae22256f406921acdc67f
Dug2555 <59838706+Dug2555@users.noreply.github.com>
2023-01-17 18:10:19 -0600

    Added first spell

--- Make_Files.py
+++ Make_Files.py
@@ -1,4 +1,6 @@
 import csv
+import os
+
 data = []
 with open("dnd-spells.csv", "r", encoding='utf-8') as file:
     csvreader = csv.reader(file)
@@ -7,3 +9,37 @@ with open("dnd-spells.csv", "r", encoding='utf-8') as file:
 
 
 print(data[0])
+
+try:
+    os.mkdir("./Spells")
+except OSError as error: 
+    print("Already made")
+
+# for i in data:
+    i = data[1]
+    f = open("Spells/" + i[0] + ".html", "w")
+    f.write("<!DOCTYPE html>\n")
+    f.write("<html>\n")
+    f.write("<body>\n")
+    f.write("<h2>" + i[0] + "</h2>\n")
+    f.write("<h3>" + i[2] + " " + i[1] + "</h3>\n")
+    f.write("<h4>" + i[3] + " " + i[4] + " " + i[5] + "</h4>\n")
+    f.write("<h4>" + i[6] + " ")
+    if i[7] == 1: 
+        f.write("V ")
+    if i[8] == 1: 
+        f.write("S ")
+    if i[9] == 1: 
+        f.write("M ")    
+    f.write(i[10] + "</h4>\n")
+    f.write("<p>" + i[11] + "</p>\n")
+    f.write("</html>\n")
+
+
+
+
+
+
+
+
+
