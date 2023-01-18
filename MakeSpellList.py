a8e364e11ee07620393b2d795e742e498bad070d
Dug2555 <59838706+Dug2555@users.noreply.github.com>
2023-01-18 00:59:26 -0600

    Accidenitly removed spell background

--- MakeSpellList.py
+++ MakeSpellList.py
@@ -7,6 +7,7 @@ f.write("<!DOCTYPE html>\n")
 f.write("<html>\n")
 f.write("<h1>List of all Spells</h1>\n")
 f.write("<body>\n")
+f.write("<style>\n body{\n background-image: url('BG2.png');\n}\n</style>\n")
 for i in files:
     name = i.replace('.html', '')
     f.write("<p><a href=\"/Spells/" + i + "\">" + name + "</a></p>\n")
