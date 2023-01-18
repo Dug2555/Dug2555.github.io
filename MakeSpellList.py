a854125734185ee4af6701e438c0a4314f1e7528
Dug2555 <59838706+Dug2555@users.noreply.github.com>
2023-01-18 00:56:21 -0600

    Fixed a bug with Spell list

--- MakeSpellList.py
+++ MakeSpellList.py
@@ -9,4 +9,4 @@ f.write("<h1>List of all Spells</h1>\n")
 f.write("<body>\n")
 for i in files:
     name = i.replace('.html', '')
-    f.write("<p><a href=\"/Spells/" + i + ".html\">" + name + "</a></p>\n")
+    f.write("<p><a href=\"/Spells/" + i + "\">" + name + "</a></p>\n")
