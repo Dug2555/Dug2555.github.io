450869044267c823fb275ef9d82004ec3ebea9c3
LoweD1 <douglas.lowe@jacobs.com>
2023-01-18 16:27:15 -0600

    added writing to a database

---
+++ Database.py
@@ -0,0 +1,10 @@
+import boto3
+
+client = boto3.client('dynamodb',
+aws_access_key_id='',
+aws_secret_access_key='',
+region_name='us-east-1')
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+AKIAY67VEAJ5WGTSUVEJ
+TgqF1KnQtDwp7WikGbFYbbormX7avroGSAXNl+q5
